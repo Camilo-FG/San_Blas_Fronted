a54407d87ccd0ed33b86bf3fb4f6a302d7592197
Keril-valle <kvalle1306@gmail.com>
2026-07-10 14:16:46 -0600

    para cambiar urls de axinos para rutas del back

--- SanBlas-Frontend/src/config/api.ts
+++ SanBlas-Frontend/src/config/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_URL ?? "http://localhost:5146/api";
+  import.meta.env.VITE_API_URL ?? "http://localhost:3000/api";
 
 export const FILES_BASE_URL = API_BASE_URL.replace(/\/api\/?$/, "");
 
